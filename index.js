3a7001b6af5769eeb60f79283351e492f000cac3
Kyle Bradford <kfoxb@kfoxb.com>
2017-06-07 10:35:52 -0600

    changed index.js to listen for tcp connections

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const express = require('express');
-const app1 = express();
-const app2 = express();
+const net = require('net');
 
-app1.all('*', (req, res) => {
-  console.log('this is req', req);
+const server = net.createServer();
+
+const handleConnection = (conn) => {
+  let remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
+  console.log(`connection from ${remoteAddress}`);
+
+  conn.on('data', (data) => {
+    console.log(`incoming data from ${remoteAddress}: ${data}`);
+    conn.write(data);
+  });
+
+  conn.once('closed', () => {
+    console.log(`connection from ${remoteAddress} closed`);
+  });
+
+  conn.on('error', (err) => {
+    console.log(`there was an error while listening to connection from ${remoteAddress}`);
+  });
+};
+
+const handleError = (err) => {
+  console.log('there was a connection error', error);
+};
+
+server.on('connection', handleConnection);
+
+server.on('error', handleError);
+
+server.listen(21030, () => {
+  console.log('listening ', server.address())
 });
-
-app2.all('*', (req, res) => {
-  console.log('this is req', req);
-});
-
-app1.listen(21030);
-app2.listen(21031)
